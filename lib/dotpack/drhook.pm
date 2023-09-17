b5aae9ac27214e7fe9b11f293cf39d9ca311d17e
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-09-17 13:24:44 +0000

    Split dotpack in lib/dotpack

---
+++ lib/dotpack/drhook.pm
@@ -0,0 +1,37 @@
+package dotpack::drhook;
+
+use strict;
+
+sub read
+{
+  shift;
+  my ($f) = @_; 
+
+  my @line = do { my $fh = 'FileHandle'->new ("<$f"); <$fh> };
+  shift (@line) for (1 .. 13); 
+
+  my %Name;
+
+  for my $line (@line)
+    {   
+      chomp ($line);
+
+      $line =~ s/^\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+//go;
+      my ($Rank, $Time, $Cumul, $Self, $Total, $Calls, $SelfPerCall, $TotalPerCall) = ($1, $2, $3, $4, $5, $6, $7, $8);
+
+
+      $line =~ s/^\*//o;
+      my ($Name, $Thread);
+
+      unless (($Name, $Thread) = ($line =~ m/^(.*)\@(\d+)$/o))
+        {
+          ($Name, $Thread) = ($line, 1); 
+        }
+
+      $Name{$Name} = 1;
+    }   
+
+  return \%Name;
+}
+
+1;
